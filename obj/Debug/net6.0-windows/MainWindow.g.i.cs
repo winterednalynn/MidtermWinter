b5c34836459b51efe2af5dc6545fd8082c7ba642
winterednalynn <Ednalynnnl@gmail.com>
2023-03-16 21:44:37 -0700

    Initial commit

--- obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -125,7 +125,7 @@ namespace MidtermWinter {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/MidtermWinter;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/MidtermWinter;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
